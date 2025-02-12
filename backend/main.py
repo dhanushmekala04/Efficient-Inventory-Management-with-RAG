02acf7233287e757b9ab893a03641e0ecb079f36
DHANUSH MEKALA <dhanushmekala04@gmail.com>
2025-02-12 12:54:08 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -152,4 +152,4 @@ async def generate_response(query_request: QueryRequest):
 # Run the FastAPI server
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=1000, reload=True)
